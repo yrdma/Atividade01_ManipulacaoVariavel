2a8efad28db4e37c6380af07240b82cc6bb6e1b0
GabrielMombelli <gabriel.mombelli9@gmail.com>
2024-08-19 21:27:17 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    int id = 16;
+    int id2 = 27;
+    double altura = 1.75;
+    String nome = "Ygor";
+    boolean estudante = true;
+
+
+    int somaId = id + id2;
+    double multiAltura = altura * 2;
+
+    System.out.println("Olá" + nome );
+    System.out.println("Idade: " + id);
+    System.out.println("Altura dobrada: " + multiAltura);
+    System.out.println("Altura: " + altura);
+    System.out.println("Soma das Idades: " + somaId);
+    System.out.println("É estudante: " + estudante);
+
+
+
+
+
+
+
+
+    }
+}
